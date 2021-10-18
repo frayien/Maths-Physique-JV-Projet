7a1b1029a902ebfa6b240e8f45dfb5fe8e90b595
frayien <garbani.adrien@yahoo.fr>
2021-10-18 10:48:31 -0400

    particle contact declaration

--- include/physics/ParticleContact.hpp
+++ include/physics/ParticleContact.hpp
@@ -1,13 +1,42 @@
 #ifndef FRAYIEN_PARTICLECONTACT
 #define FRAYIEN_PARTICLECONTACT
 
+#include <array>
+
+#include "physics/Particle.hpp"
+
 class ParticleContact
 {
 private:
+    std::array<Particle*, 2> m_particles;
+
+    // value between 0 - 1
+    // define the elasticness of the collision
+    float m_restitution;
+
+    // distance of penetration of the contact
+    float m_penetration;
+
+    // contact normal
+    Vector3f m_normal;
     
 public:
-    ParticleContact();
+    ParticleContact(Particle* particleA, Particle* particleB);
     virtual ~ParticleContact();
+
+    void resolve(float deltaTime);
+    float calculateSeparatingVelocity();
+
+    inline void setRestitution(float rest) { m_restitution = rest; }
+    inline float getRestitution() { return m_restitution; }
+
+    inline float getPenetration() { return m_penetration; }
+
+    inline Vector3f getNormal() { return m_normal; }
+
+private:
+    void resolveVelocity();
+    void resolveInterpenetration();
 };
 
 #endif // FRAYIEN_PARTICLECONTACT
--- src/physics/ParticleContact.cpp
+++ src/physics/ParticleContact.cpp
@@ -1,9 +1,32 @@
 #include "physics/ParticleContact.hpp"
 
-ParticleContact::ParticleContact()
+ParticleContact::ParticleContact(Particle* particleA, Particle* particleB) :
+    m_particles { particleA, particleB }
 {
+    
 }
 
 ParticleContact::~ParticleContact()
 {
+}
+
+void resolve(float deltaTime)
+{
+    resolveVelocity();
+    resolveInterpenetration();
+}
+
+float calculateSeparatingVelocity()
+{
+
+}
+
+void resolveVelocity()
+{
+
+}
+
+void resolveInterpenetration()
+{
+
 }
