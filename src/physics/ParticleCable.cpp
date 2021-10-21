d0409d6c880018684177e2f9e27b0130c6fd0d9d
koechslin <killian.oechslin@insa-lyon.fr>
2021-10-21 14:10:54 -0400

    fix particle cable and rod

--- src/physics/ParticleCable.cpp
+++ src/physics/ParticleCable.cpp
@@ -1,6 +1,7 @@
 #include "physics/ParticleCable.hpp"
 
-ParticleCable::ParticleCable(float maxLength, float restitution) : 
+ParticleCable::ParticleCable(Particle* particleOne, Particle* particleTwo, float maxLength, float restitution) :
+	ParticleLink{particleOne, particleTwo},
 	m_maxLength {maxLength},
 	m_restitution {restitution}
 {
@@ -9,7 +10,7 @@ ParticleCable::ParticleCable(float maxLength, float restitution) :
 void ParticleCable::addContact(std::vector<ParticleContact>& contacts) const {
 
 	if (getCurrentLength() > m_maxLength) {
-		Vector3f vectorToNormalize{ m_particles[0]->getPosition() - m_particles[1]->getPosition() };
+		Vector3f vectorToNormalize{ m_particles[1]->getPosition() - m_particles[0]->getPosition() };
 
 		contacts.emplace_back(m_particles[0], m_particles[1], std::abs(getCurrentLength() - m_maxLength), vectorToNormalize.normalize(), m_restitution);
 	}
