9bf5134a9fcbbf5085556bc8f0c8cb6ddd60d39a
koechslin <killian.oechslin@insa-lyon.fr>
2021-11-24 16:37:12 -0500

    add BoundingVolumeSphere

---
+++ include/collision/BoundingVolumeSphere.hpp
@@ -0,0 +1,25 @@
+#ifndef MPJVP_BOUNDINGVOLUMESPHERE
+#define MPJVP_BOUNDINGVOLUMESPHERE
+
+#include "physics/RigidBody.hpp"
+#include "physics/Vector3f.hpp"
+
+class BoundingVolumeSphere
+{
+private:
+    Vector3f m_position;
+    float m_radius;
+    RigidBody * m_rigidbody;
+    Vector3f m_offset;
+
+public:
+    BoundingVolumeSphere(Vector3f position, float radius, RigidBody * rigidbody = nullptr, Vector3f offset = {0.0f, 0.0f, 0.0f});
+    virtual ~BoundingVolumeSphere() = default;
+
+    bool intersects(BoundingVolumeSphere* other);
+
+    inline Vector3f getPosition() { return m_position; }
+    inline float getRadius() { return m_radius; }
+};
+
+#endif // MPJVP_BOUNDINGVOLUMESPHERE
---
+++ src/collision/BoundingVolumeSphere.cpp
@@ -0,0 +1,16 @@
+#include "collision/BoundingVolumeSphere.hpp"
+
+BoundingVolumeSphere::BoundingVolumeSphere(Vector3f position, float radius, RigidBody * rigidbody, Vector3f offset) :
+    m_position{position},
+    m_radius{radius},
+    m_rigidbody{rigidbody},
+    m_offset{offset}
+{
+
+}
+
+bool BoundingVolumeSphere::intersects(BoundingVolumeSphere* other)
+{
+    float sqrDistance = (m_position - other->m_position).sqrNorm();
+    return sqrDistance <= ((m_radius + other->m_radius) * (m_radius + other->m_radius));
+}
